ebf972e8011f659b31c855a5802eeacbda262cbe
Muhammad Kabir Abdullahi <mkabiru.abd@gmail.com>
2025-07-05 19:50:38 +0200

    feat: Enhance Bed Time Book component with double page mode and additional pages

--- ABC_Flash_Cards_Angular/ABC_Flash_Cards_Angular/src/app/bed-time-book/bed-time-book.component.ts
+++ ABC_Flash_Cards_Angular/ABC_Flash_Cards_Angular/src/app/bed-time-book/bed-time-book.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +7,7 @@ interface BedTimeBookPage {
   title: string;
   text: string;
   image: string;
+  isCover?: boolean;
 }
 
 @Component({
@@ -17,6 +19,12 @@ interface BedTimeBookPage {
 })
 export class BedTimeBookComponent {
   pages: BedTimeBookPage[] = [
+    {
+      title: "Kids' Bedtime Adventure",
+      text: 'A magical storybook for sweet dreams. Tap next to begin your adventure!',
+      image: 'assets/bedtime/cover.png',
+      isCover: true
+    },
     {
       title: 'Good Night, Moon',
       text: 'The moon shines bright, lighting up the night. Time to snuggle in bed and say good night.',
@@ -91,20 +99,104 @@ export class BedTimeBookComponent {
       title: 'Morning Awaits',
       text: 'Sleep tight, morning will come with new adventures.',
       image: 'assets/bedtime/morning.png'
+    },
+    // Add more creative pages to reach 20
+    {
+      title: 'Rainbow Dreams',
+      text: 'Rainbows fill your dreams with color and joy.',
+      image: 'assets/bedtime/rainbow.png'
+    },
+    {
+      title: 'Friendly Fireflies',
+      text: 'Fireflies dance in the night, lighting your way to dreamland.',
+      image: 'assets/bedtime/fireflies.png'
+    },
+    {
+      title: 'Snuggly Puppy',
+      text: 'A puppy curls up at your feet, ready to sleep too.',
+      image: 'assets/bedtime/puppy.png'
+    },
+    {
+      title: 'Starry Blanket',
+      text: 'A blanket of stars covers you as you rest.',
+      image: 'assets/bedtime/starblanket.png'
+    },
+    {
+      title: 'Sweet Dreams',
+      text: 'Close your eyes and drift into sweet dreams. Good night!',
+      image: 'assets/bedtime/sweetdreams.png'
+    },
+    {
+      title: 'The End',
+      text: 'Thank you for reading! Sleep well and have wonderful dreams. Good night!',
+      image: 'assets/bedtime/backcover.png',
+      isCover: true // Use isCover to style as a back cover if needed
     }
   ];
 
   currentPage = 0;
+  doublePageMode = true;
+
+  get isCover() {
+    return this.currentPage === 0;
+  }
+
+  get leftPageIndex() {
+    // After cover, left page is always odd index
+    return this.currentPage;
+  }
+
+  get rightPageIndex() {
+    return this.currentPage + 1;
+  }
 
   nextPage() {
-    if (this.currentPage < this.pages.length - 1) {
-      this.currentPage++;
+    if (this.isCover) {
+      this.currentPage = 1;
+    } else if (this.doublePageMode) {
+      if (this.currentPage + 2 < this.pages.length) {
+        this.currentPage += 2;
+      } else if (this.currentPage + 1 < this.pages.length) {
+        this.currentPage += 1;
+      }
+    } else {
+      if (this.currentPage + 1 < this.pages.length) {
+        this.currentPage += 1;
+      }
     }
   }
 
   prevPage() {
-    if (this.currentPage > 0) {
-      this.currentPage--;
+    if (this.isCover) {
+      return;
+    }
+    if (this.doublePageMode) {
+      if (this.currentPage - 2 >= 1) {
+        this.currentPage -= 2;
+      } else {
+        this.currentPage = 0;
+      }
+    } else {
+      if (this.currentPage - 1 >= 1) {
+        this.currentPage -= 1;
+      } else {
+        this.currentPage = 0;
+      }
+    }
+  }
+
+  togglePageMode() {
+    this.doublePageMode = !this.doublePageMode;
+    // Adjust currentPage to be valid for the new mode
+    if (this.doublePageMode && this.currentPage === 0) {
+      // Stay on cover
+      return;
+    }
+    if (this.doublePageMode && this.currentPage % 2 === 0 && this.currentPage !== 0) {
+      // Always start left page on odd index after cover
+      this.currentPage = this.currentPage;
+    } else if (this.doublePageMode && this.currentPage % 2 === 1) {
+      this.currentPage = this.currentPage - 1;
     }
   }
 }
