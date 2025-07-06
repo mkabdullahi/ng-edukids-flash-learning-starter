4b999c5b04caddc6a120fb30e4566627021d49e3
Muhammad Kabir Abdullahi <mkabiru.abd@gmail.com>
2025-07-06 16:59:17 +0200

    fix: angular json project paths

---
+++ .angular/cache/20.0.4/ng-edukids-flash-learning-starter/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-XJIVC556.js";
+import "./chunk-L7AQJQLR.js";
+import "./chunk-SESUV4G6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
